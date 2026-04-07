67024f91a949ae6547f6a0d34530eb41c2c8f47f
Jonathan Brink <jonathandavidbrink@gmail.com>
2026-04-07 11:04:28 -0400

    upgrade dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "export",
@@ -8,6 +9,9 @@ const nextConfig: NextConfig = {
     silenceDeprecations: ["legacy-js-api"],
   },
   images: { unoptimized: true },
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
